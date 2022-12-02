e4e09d1329c8fafaaa38694aab4d726b45d25dff
Terence Kong <tko.developer.02@gmail.com>
2022-12-03 00:57:46 +0800

    day 2 part 2 solution

---
+++ solutions/day2_part2.ts
@@ -0,0 +1,45 @@
+import { input } from "../inputs/day2";
+
+type Enemy = "A" | "B" | "C";
+type Outcome = "X" | "Y" | "Z";
+
+function val(outcome: Outcome) {
+    if (outcome === "X") return 0;
+    if (outcome === "Y") return 3;
+    if (outcome === "Z") return 6;
+    return 0;
+}
+
+const ROCK = 1;
+const PAPER = 2;
+const SCISSORS = 3;
+
+function score(outcome: Outcome, enemy: Enemy) {
+    // outcome WIN
+    if (outcome === "X" && enemy === "A") return PAPER + val(outcome);
+    if (outcome === "X" && enemy === "B") return SCISSORS + val(outcome);
+    if (outcome === "X" && enemy === "C") return ROCK + val(outcome);
+    // outcome DRAW
+    if (outcome === "Y" && enemy === "A") return ROCK + val(outcome);
+    if (outcome === "Y" && enemy === "B") return PAPER + val(outcome);
+    if (outcome === "Y" && enemy === "C") return SCISSORS + val(outcome);
+    // outcome LOSE
+    if (outcome === "Z" && enemy === "A") return SCISSORS + val(outcome);
+    if (outcome === "Z" && enemy === "B") return ROCK + val(outcome);
+    if (outcome === "Z" && enemy === "C") return PAPER + val(outcome);
+
+    return 0;
+}
+
+let total = 0;
+const rounds = input.split("\n");
+console.log(rounds);
+for (let i = 0; i < rounds.length; i++) {
+    if (rounds[i] === "") {
+    } else {
+        const [enemy, outcome] = rounds[i].split(" ");
+        total += score(outcome as Outcome, enemy as Enemy);
+    }
+}
+
+console.log(total);
